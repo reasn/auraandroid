0259effe5f6d6a2254499b066d634bd99834ae96
reasn <github@reasn.de>
2018-02-16 20:35:09 +0100

    feat: styles PermissionMissingActivity

--- app/src/main/java/io/auraapp/auraandroid/PermissionMissingActivity.java
+++ app/src/main/java/io/auraapp/auraandroid/PermissionMissingActivity.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import io.auraapp.auraandroid.common.EmojiHelper;
 import io.auraapp.auraandroid.common.PermissionHelper;
 import io.auraapp.auraandroid.main.MainActivity;
 
@@ -24,21 +28,30 @@ public class PermissionMissingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // TODO add toolbar with icon
-
-        // TODO style this
-
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_permission_missing);
 
-        findViewById(R.id.show_permission_dialog).setOnClickListener((View $) -> {
+        // Create toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+
+        ((TextView) findViewById(R.id.permission_explanation)).setText(EmojiHelper.replaceAppEmoji(getString(R.string.ui_permissionsMissing_text)));
+        ((TextView) findViewById(R.id.permission_second_text)).setText(EmojiHelper.replaceAppEmoji(getString(R.string.ui_permissionsMissing_second_text)));
+
+        Button showPermisisonDialogButton = findViewById(R.id.show_permission_dialog);
+        showPermisisonDialogButton.setText(EmojiHelper.replaceAppEmoji(getString(R.string.ui_permissionsMissing_request)));
+        showPermisisonDialogButton.setOnClickListener((View $) -> {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_REQUEST);
             } else {
                 throw new RuntimeException("Attempted to show permission dialog for Android < M");
             }
         });
-        findViewById(R.id.show_app_settings).setOnClickListener((View $) -> {
+        Button showAppSettingsButton = findViewById(R.id.show_app_settings);
+        showAppSettingsButton.setText(EmojiHelper.replaceAppEmoji(getString(R.string.ui_permissionsMissing_appSettings)));
+        showAppSettingsButton.setOnClickListener((View $) -> {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
                 Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
--- app/src/main/java/io/auraapp/auraandroid/common/EmojiHelper.java
+++ app/src/main/java/io/auraapp/auraandroid/common/EmojiHelper.java
@@ -5,6 +5,7 @@ public class EmojiHelper {
     public static String replaceAppEmoji(String input) {
         return input.replaceAll(":fire:", "🔥")
                 .replaceAll(":thought_balloon:", "💭")
+                .replaceAll(":pencil:", "📝")
                 .replaceAll(":heavy_plus_sign:", "➕");
     }
 }
